4f54945fc45732341f4452efc5193780c228713e
Auric Vente <mprops@protonmail.com>
2025-02-05 11:34:50 -0600

    Mods

---
+++ static/js/base.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+  let captcha = document.querySelector(`#captcha-text`)
+  captcha.placeholder = `Enter the captcha`
+}
+
+function validate() {
+  let captcha = document.querySelector(`#captcha-text`)
+
+  if (captcha) {
+      if (!captcha.value.trim()) {
+          return false
+      }
+  }
+
+  let code = document.querySelector(`#code`)
+
+  if (code) {
+      if (!code.value.trim()) {
+          return false
+      }
+  }
+
+  let file = document.querySelector(`#file`)
+
+  if (file.files.length > 0) {
+      let file = file.files[0]
+
+      if (file.size > max_size) {
+          return false
+      }
+  }
+
+  return true
+}
