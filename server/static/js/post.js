5487e85f9046d48bb67e224b3ae985f01788cae2
Auric Vente <mprops@protonmail.com>
2025-02-12 02:53:11 -0600

    Mods

---
+++ server/static/js/post.js
@@ -0,0 +1,91 @@
+MINUTE = 60000
+HOUR = MINUTE * 60
+DAY = HOUR * 24
+MONTH = DAY * 30
+YEAR = DAY * 365
+
+window.onload = function() {
+  let ago = document.querySelector(`#ago`)
+  vars.date_ms = vars.date * 1000
+
+  if (ago) {
+    let delay = 30
+
+    setInterval(function() {
+      ago.textContent = timeago(vars.date_ms)
+    }, 1000 * delay)
+  }
+}
+
+function timeago(date) {
+  let level = 0
+  let diff = Date.now() - date
+  let result
+
+  if (diff < MINUTE) {
+    result = `just now`
+    level = 1
+  }
+  else if (diff < HOUR) {
+    let n = parseInt(diff / MINUTE)
+
+    if (n === 1) {
+      result = `${n} min ago`
+    }
+    else {
+      result = `${n} mins ago`
+    }
+
+    level = 2
+  }
+  else if ((diff >= HOUR) && (diff < DAY)) {
+    let n = parseInt(diff / HOUR)
+
+    if (n === 1) {
+      result = `${n} hr ago`
+    }
+    else {
+      result = `${n} hrs ago`
+    }
+
+    level = 3
+  }
+  else if ((diff >= DAY) && (diff < MONTH)) {
+    let n = parseInt(diff / DAY)
+
+    if (n === 1) {
+      result = `${n} day ago`
+    }
+    else {
+      result = `${n} days ago`
+    }
+
+    level = 4
+  }
+  else if ((diff >= MONTH) && (diff < YEAR)) {
+    let n = parseInt(diff / MONTH)
+
+    if (n === 1) {
+      result = `${n} month ago`
+    }
+    else {
+      result = `${n} months ago`
+    }
+
+    level = 5
+  }
+  else if (diff >= YEAR) {
+    let n = parseInt(diff / YEAR)
+
+    if (n === 1) {
+      result = `${n} year ago`
+    }
+    else {
+      result = `${n} years ago`
+    }
+
+    level = 6
+  }
+
+  return result
+}
