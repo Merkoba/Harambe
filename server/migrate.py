d1171ca4cc16add2fceb8ab5361043233cee3550
Auric Vente <mprops@protonmail.com>
2025-02-16 08:26:26 -0600

    Posts

--- server/migrate.py
+++ server/migrate.py
@@ -1,16 +1,18 @@
-# Modules
-import database
+def migrate_files_to_posts() -> None:
+    conn, c = database.get_conn()
 
+    # Check if the 'files' table exists
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='files'")
+    if c.fetchone():
+        # Copy data from 'files' to 'posts'
+        c.execute("""
+            INSERT INTO posts (name, ext, date, title, views, original, username, uploader, mtype, view_date, listed, size, sample)
+            SELECT name, ext, date, title, views, original, username, uploader, mtype, view_date, listed, size, sample
+            FROM files
+        """)
+        conn.commit()
 
-conn, c = database.get_conn()
+    conn.close()
 
-c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='files'")
-
-if c.fetchone():
-    c.execute("""
-        INSERT INTO posts
-        SELECT * FROM files
-    """)
-    conn.commit()
-
-conn.close()
+# Call the migration function
+migrate_files_to_posts()
