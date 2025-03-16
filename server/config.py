d347a24f57e1655991c71368c17402a5b6247963
Auric Vente <mprops@protonmail.com>
2025-03-16 10:17:59 -0600

    Mods

--- server/config.py
+++ server/config.py
@@ -384,7 +384,7 @@ class Config:
         self.gif_magic_height = 480
 
         # Frames per second for gif magic
-        self.gif_magic_fps = 2
+        self.gif_magic_fps = 1.5
 
         # Background color for gif magic
         self.gif_magic_color = "black"
