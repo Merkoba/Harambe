d8461a07ed435bd0d8cf787e14144b49c7d85b4f
Auric Vente <mprops@protonmail.com>
2025-08-13 13:27:02 -0600

    Icon mod

--- server/icons.py
+++ server/icons.py
@@ -110,7 +110,7 @@ def fill_icons(config: Config) -> None:
     config.icons["links"] = "🔗"
 
     # Icon for you
-    config.icons["you"] = "👾"
+    config.icons["you"] = "😸"
 
     # Icon for jump
     config.icons["jump"] = "⏫"
